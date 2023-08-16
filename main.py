03ad7ec98d478ae90d89e9dd4d94f8375305e315
Admin <dinirr122@gmail.com>
2023-08-17 02:22:49 +0500

    ready 60% of bot

---
+++ main.py
@@ -0,0 +1,19 @@
+from database import start_db
+from aiogram.utils import executor
+from create_bot import dp, loop
+from handlers.start_handler import check_and_send
+
+def register_all_handlers_client(dp):
+    from handlers import start_handler, admin
+    start_handler.register_handlers_client(dp)
+    admin.register_handlers_client(dp)
+async def on_startup(_):
+    start_db.db_start()
+    register_all_handlers_client(dp)
+    loop.create_task(check_and_send())
+
+
+
+if __name__ == "__main__":
+    # dp.middleware.setup(ThrottlingMiddleware())
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
